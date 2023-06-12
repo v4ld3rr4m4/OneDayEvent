836aafc9bdcd1d9fb99cf682edf6313782794316
ismaelv <ismaelv@noritex.com>
2023-06-12 18:41:41 -0500

    First 3 main files

---
+++ event.js
@@ -0,0 +1,23 @@
+const aEventTitle = "Open Free Sofware Day"
+
+activities = [
+    {
+     "schedule":12,
+     "title":"RECESO",
+     "speaker": " ",
+     "tags":['docker','devops']
+    },
+    {
+     "schedule":9,
+     "title":"Dockerinzando cron",
+     "speaker": "Ismael Valderrama",
+     "tags":['docker','devops']
+    },
+    {
+     "schedule":16,
+     "title":"API REST - open doors  for haking, lets talk about JWT",
+     "speaker": "Lucas Modrick",
+     "tags":['docker','devops']
+    }
+]
+ 
